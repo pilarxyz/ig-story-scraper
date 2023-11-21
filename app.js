19ff0cbc6ae808f71a703989ff917aeebfc51622
pilarxyz <pilar@student.uns.ac.id>
2023-11-21 21:19:35 +0700

    get username id

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
+const { scrapeId, scrapeStory } = require("./lib/scraper");
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Halaman Utama' });
+app.get("/", (req, res) => {
+  res.render("index", { title: "IG Scraper" });
+});
+// TypeError: Cannot read properties of undefined (reading 'username')
+app.post("/user", (req, res) => {
+  const username = req.body.username;
+  const id = scrapeId(username);
+    const story = scrapeStory(id);
+    res.render("user", { title: "IG Scraper", username, id, story });
+    
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
